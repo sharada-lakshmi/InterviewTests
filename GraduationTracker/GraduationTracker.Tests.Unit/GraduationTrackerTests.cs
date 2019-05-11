0f49a3e66a721d3b760d59a4178ab2bfd9812d75
sharada-lakshmi <saradalakshmi.mandadi@gmail.com>
2019-05-12 01:31:27 +0530

    Update GraduationTrackerTests.cs
    1.Added one new test case.
    2.Modified some of the comments in existing Test methods.
    3.Added break statement in iterations where ever it is necessary.

--- GraduationTracker/GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
+++ GraduationTracker/GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="GraduationTrackerTests.cs" company="Xello">
+// <copyright file="GraduationTrackerTests.cs" company="Xello">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
@@ -16,7 +16,7 @@ namespace GraduationTracker.Tests.Unit
     public class GraduationTrackerTests
     {
         /// <summary>
-        /// test if the single student is graduated
+        /// test if the student is graduated
         /// </summary>
         [TestMethod]
         public void TestStudentHasGraduated()
@@ -49,13 +49,13 @@ namespace GraduationTracker.Tests.Unit
 
             foreach (var student in students)
             {
-                // iteam 1 is the boolean value if the student is graduated or not based on the business logic
+                // item 1 is the boolean value if the student is graduated or not based on the business logic
                 Assert.IsTrue(tracker.HasGraduated(student, diploma).Item1 == true);
             }
         }
 
         /// <summary>
-        /// test if the any student form the list is graduated
+        /// test if the student is not graduated
         /// </summary>
         [TestMethod]
         public void TestStudentHasNotGraduated()
@@ -86,13 +86,13 @@ namespace GraduationTracker.Tests.Unit
 
             foreach (var student in students)
             {
-                // iteam 1 is the boolean value if the student is graduated or not based on the business logic
+                // item 1 is the boolean value if the student is graduated or not based on the business logic
                 Assert.IsTrue(tracker.HasGraduated(student, diploma).Item1 == false);
             }
         }
 
         /// <summary>
-        /// test student is not graduated
+        /// test any student is graduated from the list
         /// </summary>
         [TestMethod]
         public void TestAnyStudentHasGraduated()
@@ -158,13 +158,15 @@ namespace GraduationTracker.Tests.Unit
             bool isGraduated = false;
             foreach (var student in students)
             {
-                // iteam 1 is the boolean value if the student is graduated or not based on the business logic
+                // item 1 is the boolean value if the student is graduated or not based on the business logic
                 if (tracker.HasGraduated(student, diploma).Item1 == true)
                 {
                     isGraduated = true;
+                    break;
                 }
-                Assert.IsTrue(isGraduated);
             }
+
+            Assert.IsTrue(isGraduated);
         }
 
         /// <summary>
@@ -235,14 +237,93 @@ namespace GraduationTracker.Tests.Unit
             bool isGraduated = false;
             foreach (var student in students)
             {
-                // iteam 1 is the boolean value if the student is graduated or not based on the business logic
+                // item 1 is the boolean value if the student is graduated or not based on the business logic
+                if (tracker.HasGraduated(student, diploma).Item1 == true)
+                {
+                    isGraduated = true;
+                    break;
+                }
+            }
+
+            Assert.IsFalse(isGraduated);
+        }
+
+        /// <summary>
+        /// test all students are graduated from the list
+        /// </summary>
+        [TestMethod]
+        public void TestAllStudentsHasGraduated()
+        {
+            var tracker = new GraduationTracker();
+
+            var diploma = new Diploma
+            {
+                Id = 1,
+                Credits = 4,
+                Requirements = new int[] { 100, 102, 103, 104 }
+            };
+
+            var students = new[]
+            {
+               new Student
+               {
+                   Id = 1,
+                   Courses = new Course[]
+                   {
+                        new Course{Id = 1, Name = "Math", Mark = 95 },
+                        new Course{Id = 2, Name = "Science", Mark = 95 },
+                        new Course{Id = 3, Name = "Literature", Mark = 95 },
+                        new Course{Id = 4, Name = "Physichal Education", Mark = 95 }
+                   }
+               },
+               new Student
+               {
+                   Id = 2,
+                   Courses = new Course[]
+                   {
+                        new Course{Id = 1, Name = "Math", Mark = 80 },
+                        new Course{Id = 2, Name = "Science", Mark = 80 },
+                        new Course{Id = 3, Name = "Literature", Mark = 80 },
+                        new Course{Id = 4, Name = "Physichal Education", Mark = 80 }
+                   }
+               },
+            new Student
+            {
+                Id = 3,
+                Courses = new Course[]
+                {
+                    new Course{Id = 1, Name = "Math", Mark = 50 },
+                    new Course{Id = 2, Name = "Science", Mark = 50 },
+                    new Course{Id = 3, Name = "Literature", Mark = 50 },
+                    new Course{Id = 4, Name = "Physichal Education", Mark = 50 }
+                }
+            },
+            new Student
+            {
+                Id = 4,
+                Courses = new Course[]
+                {
+                    new Course{Id = 1, Name = "Math", Mark = 40 },
+                    new Course{Id = 2, Name = "Science", Mark = 40 },
+                    new Course{Id = 3, Name = "Literature", Mark = 40 },
+                    new Course{Id = 4, Name = "Physichal Education", Mark = 40 }
+                }
+            }
+        };
+
+            var graduated = new List<Tuple<bool, STANDING>>();
+            bool isGraduated = true;
+            foreach (var student in students)
+            {
+                // item 1 is the boolean value if the student is graduated or not based on the business logic
                 if (tracker.HasGraduated(student, diploma).Item1 == false)
                 {
                     isGraduated = false;
+                    break;
                 }
-
-                Assert.IsFalse(isGraduated);
             }
+
+            Assert.IsTrue(isGraduated);
         }
     }
 }
